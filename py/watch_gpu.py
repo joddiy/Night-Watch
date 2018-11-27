69edcb38722695f744d71a55aa2cc00056dd9ef4
joddiy <joddiy@qq.com>
2018-11-27 20:37:33 +0800

    commit

--- py/watch_gpu.py
+++ py/watch_gpu.py
@@ -12,6 +12,6 @@ gpustats.print_json(fp=fp)
 
 print(fp.getvalue())
 
-r = requests.post("http://ncrs.d2.comp.nus.edu.sg/api/watch-gpu", data={"log":fp.getvalue()})
+r = requests.post("http://ncrs.d2.comp.nus.edu.sg/api/watch-gpu", data=fp.getvalue())
 
 print(r.text)
