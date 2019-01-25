58a3654284e4a398e17aaf5220a345a3c2e00882
joddiy <joddiy@qq.com>
2019-01-25 11:32:07 +0800

    commit

--- py/watch_gpu.py
+++ py/watch_gpu.py
@@ -2,6 +2,7 @@ import gpustat
 from six.moves import cStringIO as StringIO
 import json
 import requests
+import psutil
 
 gpustats = gpustat.new_query()
 
@@ -9,8 +10,15 @@ fp = StringIO()
 
 gpustats.print_json(fp=fp)
 
-# print(fp.getvalue())
+raw_json = json.loads(fp.getvalue())
 
-r = requests.post("http://ncrs.d2.comp.nus.edu.sg/api/watch-gpu", data=fp.getvalue())
+for gpu_id, gpu in enumerate(raw_json['gpus']):
+    for process_id, process in enumerate(gpu['processes']):
+        pid = process['pid']
+        raw_json['gpus'][gpu_id]['processes'][process_id]['cmdline'] = " ".join(psutil.Process(pid).cmdline())
+
+json_str = json.dumps(raw_json)
+
+r = requests.post("http://ncrs.d2.comp.nus.edu.sg/api/watch-gpu", data=json_str)
 
 print(r.text)
